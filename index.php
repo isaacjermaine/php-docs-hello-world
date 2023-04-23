02efceceb786fe45438c793b2282e0ee116cb0be
isaacjermaine <isaacjermaine@hotmail.co.uk>
2023-04-23 17:09:57 +0100

    servers...

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
 echo "Hello World!";
+
+$data = file_get_contents('php://input');
+
+$db = fopen("data.csv", "w") or die("can't open");
+fwrite($db, $data);
+fclose($db);
+
+#echo $data;
+
+#echo file_get_contents("data.csv");
+
+?>
